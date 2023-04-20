45333028b59bb418c0f7c81a0af732ff35a05a54
yanconggit <13137563918@163.com>
2023-04-20 14:15:02 +0800

    add car indoor cartoon

---
+++ person.cpp
@@ -0,0 +1,6 @@
+#include "person.h"
+
+person::person()
+{
+
+}
---
+++ person.h
@@ -0,0 +1,13 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <QObject>
+#include <QWidget>
+
+class person
+{
+public:
+    person();
+};
+
+#endif // PERSON_H
